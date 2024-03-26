5483d1cc20c723e6d618224d9904caac72b1e568
Mohnish <mohnish.a007@gmail.com>
2024-03-26 15:19:44 +0000

    get request working

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3300
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/getobject', (req, res) => {
+  res.send([
+    {
+      "id": 1,
+      "name": "A",
+    },
+    {
+      "id": 2,
+      "name": "B",
+    },
+    {
+      "id": 3,
+      "name": "C",
+    },
+  ])
 })
 
 app.listen(port, () => {
